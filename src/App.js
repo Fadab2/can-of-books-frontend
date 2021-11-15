37628ae7cdde26f42063f238c290a9574f6b02ae
Joseph Nguyen <josephnguyen2@gmail.com>
2021-11-14 20:25:58 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
 
 
   setStateOfFrom = async (title, description, stat) => {
-    let status = stat === "true";
+    let status = stat === "on";
     let email = this.props.auth0.user.email;
     let newBook = { title, description, status, email };
     let url = `${process.env.REACT_APP_SERVER_URL}/books`;
@@ -62,7 +62,8 @@ class App extends React.Component {
     }
   };
   updateBooks = async (title, description, stat) => {
-    let status = stat === "true";
+    console.log(stat)
+    let status = stat === "on";
     let id = this.state.id;
     let book = { title, description, status, email: this.props.auth0.user.email };
     const url = `${process.env.REACT_APP_SERVER_URL}/books/${id}`;
